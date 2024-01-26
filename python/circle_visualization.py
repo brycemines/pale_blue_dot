415855b501cc49d080ed5d0b817c2be85979db26
Bryce Irving <bryceirving@mines.edu>
2024-01-26 11:04:32 -0700

    Fix the circle_visualization file and notebook

--- python/circle_visualization.py
+++ python/circle_visualization.py
@@ -2,12 +2,11 @@ import drawsvg as draw
 import pandas as pd
 
 class CircleVisualization:
-    def __init__(self, methane_data, year_data):
+    def __init__(self, methane_data):
+        '''Initialize the class with the data to be visualized'''
+
         self.data = methane_data
         '''the data to be visualized'''
-
-        self.years = year_data
-        '''the years corresponding to the data'''
 
         self.max_value = max(methane_data)
         '''the maximum value in the data'''
@@ -53,11 +52,9 @@ class CircleVisualization:
 
 
     def draw(self):
+        '''Draw the overlapping concentric circles'''
         # restart with a blank canvas
         self.d = draw.Drawing(self.width*self.figure_scale, self.height*self.figure_scale, origin='center', fill='black')
-
-        # draw the background rectangle, if you want a different color
-        # self.d.append(draw.Rectangle(-200,-100,400 * self.figure_scale,200 * self.figure_scale, fill='black'))
 
         # draw the circles
         for i, value in enumerate(self.data):
@@ -66,38 +63,48 @@ class CircleVisualization:
             # increment the color
             self.increment_color(value)
 
+            # draw the circle
             color = "rgb("+str(self.red)+","+str(self.green)+","+str(self.blue)+")"
             self.d.append(draw.Circle(self.cx, self.cy, radius, fill=color))
 
-        # add a pale blue dot in the center ??
-        # self.d.append(draw.Circle(self.cx, self.cy, 1, fill='rgb(50,50,200)'))
         return self.d
 
 
     def draw_colorbar(self):
+        '''Draw the colorbar'''
         # restart with a blank canvas
         self.d = draw.Drawing(self.width*self.figure_scale, self.height*self.figure_scale, origin='center', fill='black')
 
+        # find the "max color". 
+        self.increment_color(self.max_value)
+        color_min = "rgb("+str(self.red)+","+str(self.green)+","+str(self.blue)+")"
+
+        # find the "min color"
+        self.increment_color(self.min_value)
+        color_max = "rgb("+str(self.red)+","+str(self.green)+","+str(self.blue)+")"
+
         gradient = draw.LinearGradient(250, -50, 275, 50)
-        gradient.add_stop(0, 'red', 1)
-        self.d.append(draw.rectangle(-250,-50,275,50)
+        gradient.add_stop(0, color_max, 1)
+        gradient.add_stop(1, color_min, 1)
+        self.d.append(draw.Rectangle(275,-50,25,100, stroke='black', stroke_width=0.002, fill=gradient))
         return self.d
 
     
     def increment_color(self, value: int):
-        # scale the data linearly between 0 and 175
-        self.green = (value - self.min_value - 1) / self.range * 175
-        # invert the green value
-        self.green = 175 - self.green
+        '''scale the data linearly between 0 and 175, keeping relative spacing'''
+        self.green = (value - self.min_value) / self.range * 175
 
 
     def set_circle_spacing(self, amount: int):
+        '''set the amount to increment the radius of each circle'''
         self.radius_increment = amount
 
 
     def set_circle_max_size(self, amount: int):
+        '''set the maximum radius of the largest circle'''
         self.max_radius = amount
 
     def set_figure_scale(self, amount: int):
+        '''scale the size of the figure'''
         self.figure_scale = amount
     
